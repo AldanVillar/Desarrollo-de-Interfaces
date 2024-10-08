642233b6b88b35beb0d950743c3db9430d67378d
AldanVillar <avillars23@fpcoruna.afundacion.org>
2024-10-08 14:14:29 +0200

    sprint0python Ejercicio 1

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,17 @@
+def suma(n1, n2):
+    return print(n1 + n2)
+
+
+def resta(n1, n2):
+    return print(n1 + n2)
+
+
+def multiplicacion(n1, n2):
+    return print(n1 + n2)
+
+
+def division(n1, n2):
+    if n2 == 0:
+        print("No se puede dividir entre 0")
+    else:
+        return print(n1 / n2)
