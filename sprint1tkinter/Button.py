c6fd9059a963c30387df374e27cb48042d2fe2c2
AldanVillar <avillars23@fpcoruna.afundacion.org>
2024-10-15 13:56:32 +0200

    sprint1tkinter Ejercicio 2 Button

---
+++ sprint1tkinter/Button.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+def crear_texto():
+    texto.config(text="Mensaje")
+def cerrar_ventana():
+    root.destroy()
+
+root = tk.Tk()
+root.title("Ventana")
+root.geometry("300x200")
+frame = tk.Frame(root)
+frame.pack(fill='both', expand=True, padx = 10, pady = 10)
+texto = tk.Label(root, text="")
+texto.pack()
+boton = tk.Button(frame, text = "Crear texto", command = crear_texto)
+boton.pack(pady = 5)
+boton = tk.Button(frame, text = "Cerrar ventana", command = cerrar_ventana)
+boton.pack(pady = 5)
+root.mainloop()
