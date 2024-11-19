9fe154c8c345b31e8fd00297f598f48a9aecd76a
AldanVillar <avillars23@fpcoruna.afundacion.org>
2024-11-19 14:12:48 +0100

    modelo.py

--- sprint3/modelo.py
+++ sprint3/modelo.py
@@ -9,32 +9,95 @@ class GameModel:
         self.difficulty = difficulty
         self.player_name = player_name
         self.cell_size = cell_size
-        start_time = 0
-        moves = 0
+        self.hidden_image = None
+        self.board = None
+        self.pairs_found = 0
+        self.images_loaded = False
+        self.images = []
+        self._generate_board()
+        self._load_images()
+        self.start_time = 0
+        self.moves = 0
 
     def _generate_board(self):
-        print()
+        num_pairs = (self.difficulty * self.difficulty) // 2
+        cards = list(range(num_pairs)) * 2
+        random.shuffle(cards)
 
     def _load_images(self):
-        print()
+        def load_images_thread():
+            self.hidden_image = descargar_imagen("url_base/hidden_image.png")
+            for card_id in range(self.difficulty * self.difficulty // 2):
+                img_url = f"url_base/{card_id}.png"
+                img = descargar_imagen(img_url)
+                self.images.append(img)
+        threading.Thread(target=load_images_thread, daemon=True).start()
 
     def images_are_loaded(self):
-        print()
+        return self.images_loaded
 
     def start_timer(self):
-        print()
+        self.start_time = time.time()
 
     def get_time(self):
-        print()
+        return round(time.time() - self.start_time, 2)
 
     def check_match(self, pos1, pos2):
-        print()
+        self.moves += 1
+        row1, col1 = pos1
+        row2, col2 = pos2
+        if self.board[row1][col1] == self.board[row2][col2]:
+            self.pairs_found += 1
+            return True
+        return False
 
     def is_game_complete(self):
-        print()
+        total_pairs = (self.difficulty * self.difficulty) // 2
+        return self.pairs_found == total_pairs
 
     def save_score(self):
-        print()
+        end_time = self.get_time()
+        score = {
+            "player_name": self.player_name,
+            "difficulty": self.difficulty,
+            "moves": self.moves,
+            "time": end_time,
+            "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        scores = self.load_scores()
+        if self.difficulty not in scores:
+            scores[self.difficulty] = []
+        scores[self.difficulty].append(score)
+        scores[self.difficulty] = sorted(scores[self.difficulty], key=lambda x: x["moves"])[:3]
+        with open("ranking.txt", "w") as file:
+            for difficulty in scores:
+                for score in scores[difficulty]:
+                    file.write(
+                        f"{score['player_name']} | {score['difficulty']}x{score['difficulty']} | {score['moves']} moves | {score['time']}s | {score['date']}\n")
 
     def load_scores(self):
-        print()
+        scores = {4: [], 6: [], 8: []}
+        try:
+            with open("ranking.txt", "r") as file:
+                for line in file:
+                    parts = line.strip().split(" | ")
+                    player_name = parts[0]
+                    difficulty = int(parts[1].split('x')[0])
+                    moves = int(parts[2].split(' ')[0])
+                    time = float(parts[3].split('s')[0])
+                    date = parts[4]
+
+                    score = {
+                        "player_name": player_name,
+                        "difficulty": difficulty,
+                        "moves": moves,
+                        "time": time,
+                        "date": date
+                    }
+
+                    if difficulty in scores:
+                        scores[difficulty].append(score)
+        except FileNotFoundError:
+            pass
+
+        return scores
