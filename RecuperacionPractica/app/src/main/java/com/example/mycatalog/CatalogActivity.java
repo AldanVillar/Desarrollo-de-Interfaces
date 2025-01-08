9e7663e1ecc76e2ba3de48f41f9c3da439cd96a8
AldanVillar <avillars23@fpcoruna.afundacion.org>
2025-01-08 10:23:52 +0100

    RecPract Ejercicio 2

--- RecuperacionPractica/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ RecuperacionPractica/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -3,26 +3,33 @@ package com.example.mycatalog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity {
     Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Intent myIntent = new Intent(context, CatalogActivity.class);
-        context.startActivity(myIntent);
+        Button button = findViewById(R.id.navegarDetalle);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(context, DetailActivity.class);
+                context.startActivity(myIntent);
+            }
+        });
     }
 }
--- RecuperacionPractica/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ RecuperacionPractica/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,7 +1,5 @@
 package com.example.mycatalog;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,19 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Context context = this;
+public class DetailActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Intent myIntent = new Intent(context, CatalogActivity.class);
-        context.startActivity(myIntent);
     }
 }
