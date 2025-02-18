d0e0769199847743189eee73a8777dc3846c686a
AldanVillar <avillars23@fpcoruna.afundacion.org>
2025-02-18 14:58:09 +0100

    semana 4 cuarto commit

--- MyFirebaseApp/app/src/main/java/com/example/myfirebaseapp/views/SplashActivity.java
+++ MyFirebaseApp/app/src/main/java/com/example/myfirebaseapp/views/SplashActivity.java
@@ -3,29 +3,25 @@ package com.example.myfirebaseapp.views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.example.myfirebaseapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        // Temporizador de 2 segundos
         new Handler().postDelayed(() -> {
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            Intent intent = user != null ? new Intent(SplashActivity.this, MainActivity.class) : new Intent(SplashActivity.this, LoginActivity.class);
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            Intent intent;
+            if (currentUser != null) {
+                intent = new Intent(SplashActivity.this, LoginActivity.class);
+            } else {
+                intent = new Intent(SplashActivity.this, LoginActivity.class);
+            }
             startActivity(intent);
             finish();
-        }, 2000); // 2 segundos
+        }, 2000); // 2 seconds splash screen
     }
 }
